6d05049fe3611d318fe2aeea63f7179970b0307f
Jawad Hassan <159148373+jawadjamro@users.noreply.github.com>
2024-11-04 09:57:31 -0800

    Add files via upload

---
+++ assignment-2/src/app/components/about/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './about.css'
+
+const About = () => {
+  return (
+    <div className='about-main'>
+    <div className='about-inner'>
+      This Is About
+    </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ assignment-2/src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/header/page'
+import About from './components/about/page'
+
+const page = () => {
+  return (
+    <div>
+      
+
+
+
+
+      <Header />
+      <About/>
+    </div>
+  )
+}
+
+export default page
